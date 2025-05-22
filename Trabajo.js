f5dc6a2c51d32f75856512052b377c53e588f2f9
JuanLiguK <bl0016@est.ort.edu.ar>
2025-05-22 10:42:02 -0300

    Jenny Commit

---
+++ Trabajo.js
@@ -0,0 +1,88 @@
+let alumnos 0 [
+    {
+        nombre: "Juan",
+        apellido: "Liguori",
+        DNI: 48009422,
+        anio: 4,
+        curso: "A",
+        nota: 7
+    }, {
+        nombre: "Martin",
+        apellido: "Safar",
+        DNI: 48802391,
+        anio: 4,
+        curso: "A",
+        nota: 9
+    }, {
+        nombre: "Gonza",
+        apellido: "Rey",
+        DNI: 48548534,
+        anio: 4,
+        curso: "B",
+        nota: 8
+
+    },{
+        nombre: "Facundo",
+        apellido: "Benavente",
+        DNI: 48861241,
+        anio: 4,
+        curso: "B",
+        nota: 10
+    },{
+        nombre: "Felipe",
+        apellido: "Novella",
+        DNI: 48000001,
+        anio: 5,
+        curso: "A",
+        nota: 2
+    }, {
+        nombre: "Tulio",
+        apellido: "Rubinstein",
+        DNI: 48000872,
+        anio: 5,
+        curso: "A",
+        nota: 1
+    }, {
+        nombre: "Augusto",
+        apellido: "nosecomoseescribe",
+        DNI: 48000601,
+        anio: 5,
+        curso: "B",
+        nota: 5
+    },{
+        nombre: "Tulio",
+        apellido: "Rudinstein",
+        DNI: 48000020,
+        anio: 4,
+        curso: "B",
+        nota: 6
+    },{
+        nombre: "Maximo",
+        apellido: "Stolkiner",
+        DNI: 48999999,
+        anio: 3,
+        curso: "A",
+        nota: 8
+    },{
+        nombre: "Maximiliano",
+        apellido: "Taxiner",
+        DNI: 48999998,
+        anio: 3,
+        curso: "B",
+        nota: 8
+    }, {
+        nombre: "Joan",
+        apellido: "Gerson",
+        DNI: 48999933,
+        anio: 3,
+        curso: "A",
+        nota: 7
+    }, {
+        nombre: "Jenny",
+        apellido: "Mod",
+        DNI: 48999887,
+        anio: 3,
+        curso: "B",
+        nota: 10
+    }
+]
